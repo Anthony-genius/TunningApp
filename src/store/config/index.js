3821965b3b6c06b2a3a3f4d1a41db8b2f4ae06f5
igor.shev <loyalsun94415@yandex.com>
2020-08-16 08:17:25 +0300

    csrf token

--- src/store/config/index.js
+++ src/store/config/index.js
@@ -1,6 +1,6 @@
 import { positions } from "react-alert";
-export const SERVER_PORT = "http://localhost:8000"
-// export const SERVER_PORT = "https://tunningapp.herokuapp.com"
+// export const SERVER_PORT = "http://localhost:8000"
+export const SERVER_PORT = "https://tunningapp.herokuapp.com"
 export const options = {
   timeout: 5000,
   position: positions.TOP_RIGHT
