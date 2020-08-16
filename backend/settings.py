9b53203afcc7ea1ed8629f8dfe474dc4a340cf71
igor.shev <loyalsun94415@yandex.com>
2020-08-16 19:04:12 +0300

    adding SSL mode

--- backend/settings.py
+++ backend/settings.py
@@ -125,6 +125,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'loyaltalent94415@gmail.com'
 EMAIL_HOST_PASSWORD = 'BillGates94415'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
