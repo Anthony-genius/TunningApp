272aadafcf8909aafc12fb3ac3d3e99361c50ef9
igor.shev <loyalsun94415@yandex.com>
2020-08-16 19:44:40 +0300

    update issue

--- backend/settings.py
+++ backend/settings.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # }
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'igrambilchester@gmail.com'
